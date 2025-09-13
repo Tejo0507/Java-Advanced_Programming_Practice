8c98afdd605b344a8b23ba3b36370a4f32338be3
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-09-13 23:36:27 +0530

    add this file

---
+++ VendingMachine.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class VendingMachine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Press 1 for juice or 2 for soda.");
+        int choice = sc.nextInt();
+
+        if (choice == 1) {
+            System.out.println("Dispensing juice.");
+        } else if (choice == 2) {
+            System.out.println("Dispensing soda.");
+        } else {
+            System.out.println("Invalid choice.");
+        }
+    }
+}
