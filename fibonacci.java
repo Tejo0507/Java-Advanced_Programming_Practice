eddeb794045e4595a27e0c3e0f0a7583ec6c0276
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-09-18 01:47:35 +0530

    Add File

---
+++ fibonacci.java
@@ -0,0 +1,14 @@
+public class fibonacci 
+{
+      public static void main(String args[]) 
+  {
+    int f = 0, g = 1;
+
+    for (int i = 0; i <= 5; i++) 
+    {
+      System.out.println(f);
+      f = f + g;
+      g = f - g;
+    }
+  }
+}
