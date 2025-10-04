4c8f5559152983eaa6d0f059132e1d7188258abb
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-04 21:20:28 +0530

    Update CompileTimePolymorphism.java

--- CompileTimePolymorphism.java
+++ CompileTimePolymorphism.java
@@ -1,17 +1,14 @@
 class BillingCounter {
-    // Method for 2 items
     void generateBill(int price1, int price2) {
         int total = price1 + price2;
         System.out.println("Bill for 2 items: " + total);
     }
-
-    // Method for 3 items
+    
     void generateBill(int price1, int price2, int price3) {
         int total = price1 + price2 + price3;
         System.out.println("Bill for 3 items: " + total);
     }
 
-    // Method for 4 items
     void generateBill(int price1, int price2, int price3, int price4) {
         int total = price1 + price2 + price3 + price4;
         System.out.println("Bill for 4 items: " + total);
@@ -22,9 +19,9 @@ public class CompileTimePolymorphism {
     public static void main(String[] args) {
         BillingCounter counter = new BillingCounter();
 
-        counter.generateBill(100, 200);                  // 2 items
-        counter.generateBill(50, 150, 250);             // 3 items
-        counter.generateBill(40, 60, 80, 120);          // 4 items
+        counter.generateBill(100, 200);                  
+        counter.generateBill(50, 150, 250);             
+        counter.generateBill(40, 60, 80, 120);          
     }
 }
 
