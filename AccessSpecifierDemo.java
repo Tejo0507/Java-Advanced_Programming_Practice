e09379d761377ca07e18c120a96eac6c76ce84af
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-06 00:15:01 +0530

    Update AccessSpecifierDemo.java

--- AccessSpecifierDemo.java
+++ AccessSpecifierDemo.java
@@ -2,7 +2,7 @@ package Inheritance;
 
 class Student4
 {
-    public String name;        // Public - accessible anywhere
-    protected int rollNumber;  // Protected - accessible in subclass & same package
-    String course;             // Default (package-private) - accessible in same package
-    private double marks;      // Private - accessible only within this class
+    public String name;
+    protected int rollNumber;
+    String course;
+    private double marks;
