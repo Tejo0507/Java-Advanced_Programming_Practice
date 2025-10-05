7ba1b6ee9c4bd8eb190b3ec0c8d0725a0160f1ad
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-06 00:09:33 +0530

    Update HierarchicalInheritanceDemo.java

--- HierarchicalInheritanceDemo.java
+++ HierarchicalInheritanceDemo.java
@@ -3,3 +3,16 @@ class Student2
 {
     protected String name;
     private int rollNo;
+    
+    void getStudentDetails(String name, int rollNo)
+    {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    void displayStudentDetails()
+    {
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+    }
+}
