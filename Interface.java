2375ecbf9dccc6d448afbe99ef76bdfb31ed23bb
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-15 22:22:12 +0530

    Remove comment from Rectangle class
    Removed comment about abstract method implementation.

--- Interface.java
+++ Interface.java
@@ -6,7 +6,6 @@ interface Polygon
 
 class Rectangle implements Polygon 
 {
-    // implementation of abstract method
     public void getArea(int length, int breadth) 
     {
       System.out.println("The area of the rectangle is " + (length * breadth));
