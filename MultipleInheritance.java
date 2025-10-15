f0ee50ef5944c1508be4614dba403f495349eee8
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-15 22:27:34 +0530

    Implement Animal class with eat and travel methods

--- MultipleInheritance.java
+++ MultipleInheritance.java
@@ -8,3 +8,15 @@ interface AnimalTravel
 {
     void travel();
 }
+
+class Animal implements AnimalEat, AnimalTravel 
+{
+    public void eat() 
+    {
+       System.out.println("Animal is eating");
+    }
+    public void travel() 
+    {
+       System.out.println("Animal is travelling");
+    }
+}
