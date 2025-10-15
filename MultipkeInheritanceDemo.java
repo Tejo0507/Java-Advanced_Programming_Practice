5d39c53a580b23fa0f67c82b9a9787d86b436e85
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-15 22:33:00 +0530

    Add MyClass implementing InterfaceA and InterfaceB
    Implemented MyClass to provide methods for both InterfaceA and InterfaceB.

--- MultipkeInheritanceDemo.java
+++ MultipkeInheritanceDemo.java
@@ -9,3 +9,17 @@ interface InterfaceB
 {
     void methodB();
 }
+
+class MyClass implements InterfaceA, InterfaceB
+{
+    // Must provide implementation for both methods
+    public void methodA()
+    {
+        //val=200;
+        System.out.println("Method A from Interface A "+val);
+    }
+    public void methodB()
+    {
+        System.out.println("Method B from Interface B");
+    }
+}
