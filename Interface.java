cd68ac25bb6d1cf6f05721516ab07bd0ae38f006
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-15 22:21:54 +0530

    Implement Rectangle class for Polygon interface

--- Interface.java
+++ Interface.java
@@ -3,3 +3,13 @@ interface Polygon
 {
     void getArea(int length, int breadth);
 }
+
+class Rectangle implements Polygon 
+{
+    // implementation of abstract method
+    public void getArea(int length, int breadth) 
+    {
+      System.out.println("The area of the rectangle is " + (length * breadth));
+    }
+}
+  
