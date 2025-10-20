64c9f083266556aac557a846787007bb3c4cd0e8
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-20 23:36:59 +0530

    Add constructor to ColorChoose class

--- ColourChoose.java
+++ ColourChoose.java
@@ -6,3 +6,10 @@ import javax.swing.*;
 public class ColorChoose extends JFrame implements ActionListener {    
 JButton b;    
 Container c;    
+ColorChoose(){    
+    c=getContentPane();    
+    c.setLayout(new FlowLayout());         
+    b=new JButton("color");    
+    b.addActionListener(this);         
+    c.add(b);    
+}    
