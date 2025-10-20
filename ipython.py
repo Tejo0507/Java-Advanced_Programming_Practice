7657878156b007a3f4c2135afb99b28ba102dd0a
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-20 23:26:59 +0530

    Implement error handling for dview connection
    Added error handling for obtaining view to engines.

--- ipython.py
+++ ipython.py
@@ -6,3 +6,9 @@ try:
 except Exception as e:
     print(f"Failed to connect to the cluster: {e}", file=sys.stderr)
     sys.exit(1)
+
+try:
+    dview = rc[:]
+except Exception as e:
+    print(f"Failed to get view to the engines: {e}", file=sys.stderr)
+    sys.exit(1)
