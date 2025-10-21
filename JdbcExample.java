299260dbc8effb0b7cddf83b449665ab542d2a53
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-21 23:37:19 +0530

    Add JdbcExample class with main method

--- JdbcExample.java
+++ JdbcExample.java
@@ -5,3 +5,10 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+public class JdbcExample {
+
+    public static void main(String[] args) {
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
