df8fc7afdbe98cd59aa948896c68f1c439e53991
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-22 23:59:10 +0530

    Update TestThread.java

--- TestThread.java
+++ TestThread.java
@@ -14,6 +14,25 @@ class ThreadDemo extends Thread
       }
       try {
          Thread.sleep(5000);
-      
+      } catch (InterruptedException e) 
+      {
+         e.printStackTrace();
+      }
+   }
+   public void start () {
+      super.start();
+   }
 }
-
+public class TestThread {
+   public static void main(String args[]) {
+      ThreadDemo thread1 = new ThreadDemo();
+      ThreadDemo thread2 = new ThreadDemo();
+      ThreadDemo thread3 = new ThreadDemo();
+      thread1.setPriority(Thread.MAX_PRIORITY);
+      thread2.setPriority(Thread.MIN_PRIORITY);
+      thread3.setPriority(Thread.NORM_PRIORITY);
+      thread1.start();	  
+      thread2.start();
+      thread3.start();
+   }
+}
