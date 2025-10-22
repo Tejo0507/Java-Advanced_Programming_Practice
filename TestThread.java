684916f4045fe23b8f54f778ad3ce4eba802aa59
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-22 23:58:40 +0530

    Implement ThreadDemo class with run and start methods

--- TestThread.java
+++ TestThread.java
@@ -1,1 +1,27 @@
+package Threading;
+class ThreadDemo extends Thread 
+{
+   ThreadDemo( ) 
+   {
+   }
+   public void run() 
+   {
+      System.out.println("Thread Name: " + Thread.currentThread().getName() 
+         + ", Thread Priority: " +Thread.currentThread().getPriority());
+      for(int i = 4; i > 0; i--) 
+      {
+         System.out.println("Thread: " + Thread.currentThread().getName() + ", " + i); 
+      }
+      try {
+         Thread.sleep(5000);
+      } catch (InterruptedException e) 
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+   }
+   public void start () {
+      super.start();
+   }
+}
 
