310eeb66dfb2b5bff04a2965de766bad45a6dce9
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-23 23:59:35 +0530

    Update PythonThread.py

--- PythonThread.py
+++ PythonThread.py
@@ -21,22 +21,3 @@ def print_time(threadName, delay, counter):
       print ("%s: %s" % (threadName, time.ctime(time.time())))
       counter -= 1
 
-threadLock = threading.Lock()
-threads = []
-
-# Create new threads
-thread1 = myThread(1, "Thread-1", 1)
-thread2 = myThread(2, "Thread-2", 2)
-
-# Start new Threads
-thread1.start()
-thread2.start()
-
-# Add threads to thread list
-threads.append(thread1)
-threads.append(thread2)
-
-# Wait for all threads to complete
-for t in threads:
-    t.join()
-print ("Exiting Class.SimpleClass Thread")
