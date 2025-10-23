284014cabff1939bd9895536cd25c6f793cef202
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-23 23:55:33 +0530

    Print inputs and outputs in multiprocess.py
    Add output printing for inputs and results of square function.

--- multiprocess.py
+++ multiprocess.py
@@ -7,3 +7,6 @@ if __name__ == '__main__':
     pool = multiprocessing.Pool() 
     pool = multiprocessing.Pool(processes=4) 
     inputs = [0,1,2,3,4] 
+    outputs = pool.map(square, inputs) 
+    print("Input: {}".format(inputs)) 
+    print("Output: {}".format(outputs)) 
