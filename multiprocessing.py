0bbe27aaf0e292ecf1dceb77163290a3c6049511
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-23 23:58:01 +0530

    Update multiprocessing.py

--- multiprocessing.py
+++ multiprocessing.py
@@ -4,3 +4,7 @@ class Process(multiprocessing.Process):
         def __init__(self, id): 
         super(Process, self).__init__() 
         self.id = id
+        
+        def run(self): 
+        time.sleep(1) 
+        print("I'm the process with id: {}".format(self.id)) 
