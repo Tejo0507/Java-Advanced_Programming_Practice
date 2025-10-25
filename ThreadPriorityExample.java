4ed87da49292cc84d5221aab1d1ee345a5512784
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-25 23:57:28 +0530

    Add print statements for thread priorities

--- ThreadPriorityExample.java
+++ ThreadPriorityExample.java
@@ -11,3 +11,7 @@ public static void main(String argvs[])
 ThreadPriorityExample th1 = new ThreadPriorityExample();  
 ThreadPriorityExample th2 = new ThreadPriorityExample();  
 ThreadPriorityExample th3 = new ThreadPriorityExample();  
+
+System.out.println("Priority of the thread th1 is : " + th1.getPriority());  
+System.out.println("Priority of the thread th2 is : " + th2.getPriority());  
+System.out.println("Priority of the thread th2 is : " + th2.getPriority());  
