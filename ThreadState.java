bdb5c06c8042025015cd2805d55c72baa37f98df
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-26 00:00:17 +0530

    Implement GFG class with thread sleep and error handling
    Added a runnable class with sleep and exception handling.

--- ThreadState.java
+++ ThreadState.java
@@ -1,1 +1,30 @@
+package Threading;
 
+class GFG implements Runnable 
+{
+    public void run()
+    {
+        // implementing try-catch Block to set sleep state
+        // for inactive thread
+        try 
+        {
+            Thread.sleep(102);
+        }
+        catch (InterruptedException i1) 
+        {
+            i1.printStackTrace();
+        }
+        System.out.println(
+            "The state for t1 after it invoked join method() on thread t2"
+            + " " + ThreadState.t1.getState());
+
+        // implementing try-catch block
+        try 
+        {
+            Thread.sleep(202);
+        }
+        catch (InterruptedException i2) 
+        {
+            i2.printStackTrace();
+        }
+    }
