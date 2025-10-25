f3ddd2c7a596e5a135caf31fae0091ce16675e3d
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-25 23:51:56 +0530

    Update ThreadExample2.java

--- ThreadExample2.java
+++ ThreadExample2.java
@@ -1,1 +1,12 @@
-
+package Threading;
+class NewThread implements Runnable 
+{  
+    String name;  
+    Thread thread;  
+    NewThread (String name)
+    {  
+        this.name = name;   
+        thread = new Thread(this, name);  
+        System.out.println( "A New thread: " + thread+ "is created\n" );  
+        thread.start();  
+    }  
