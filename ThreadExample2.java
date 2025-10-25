8e097ccdbbad23726f257908d040b29bfc076e24
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-25 23:52:19 +0530

    Implement thread run method with countdown

--- ThreadExample2.java
+++ ThreadExample2.java
@@ -10,3 +10,21 @@ class NewThread implements Runnable
         System.out.println( "A New thread: " + thread+ "is created\n" );  
         thread.start();  
     }  
+
+    public void run() 
+    {  
+        try 
+        {  
+            for(int j = 5; j > 0; j--) 
+            {  
+                System.out.println(name + ": " + j);  
+                Thread.sleep(1000);  
+            }  
+        }
+        catch (InterruptedException e) 
+        {  
+            System.out.println(name + " thread Interrupted");  
+        }  
+        System.out.println(name + " thread exiting.");  
+    }  
+}  
