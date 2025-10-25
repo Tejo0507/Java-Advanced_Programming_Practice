24b5848c24c2f8f37fc407a42498ff1a6e0edd54
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-25 23:59:07 +0530

    Update ThreadPriorityExample.java

--- ThreadPriorityExample.java
+++ ThreadPriorityExample.java
@@ -23,3 +23,14 @@ th3.setPriority(9);
 System.out.println("Priority of the thread th1 is : " + th1.getPriority());  
 System.out.println("Priority of the thread th2 is : " + th2.getPriority());  
 System.out.println("Priority of the thread th3 is : " + th3.getPriority());  
+
+  System.out.println("Currently Executing The Thread : " + Thread.currentThread().getName());  
+  
+System.out.println("Priority of the main thread is : " + Thread.currentThread().getPriority());  
+  
+// Priority of the main thread is 10 now  
+Thread.currentThread().setPriority(10);  
+  
+System.out.println("Priority of the main thread is : " + Thread.currentThread().getPriority());  
+}  
+}  
