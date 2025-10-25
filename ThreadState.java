8c8b021a294f6881bce8b855c5f95e67559f155b
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-26 00:02:27 +0530

    Fix string concatenation in ThreadState.java

--- ThreadState.java
+++ ThreadState.java
@@ -13,8 +13,7 @@ class GFG implements Runnable
             i1.printStackTrace();
         }
         System.out.println(
-            "The state for t1 after it invoked join method() on thread t2"
-            + " " + ThreadState.t1.getState());
+            "The state for t1 after it invoked join method() on thread t2" + " " + ThreadState.t1.getState());
         try 
         {
             Thread.sleep(202);
@@ -34,12 +33,7 @@ public class ThreadState implements Runnable
     {
         o1 = new ThreadState();
         t1 = new Thread(o1);
-        System.out.println("post-spanning, state of t1 is"
-                           + " " + t1.getState());
-        // lets invoke start() method on t1
+        System.out.println("post-spanning, state of t1 is" + " " + t1.getState());
         t1.start();
-        // Now,Thread t1 is moved to runnable state
-        System.out.println(
-            "post invoking of start() method, state of t1 is"
-            + " " + t1.getState());
+        System.out.println("post invoking of start() method, state of t1 is" + " " + t1.getState());
     }
