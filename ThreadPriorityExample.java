952d8dc3c920159ce85749919c85af3706526ee3
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-10-25 23:56:52 +0530

    Add main method to ThreadPriorityExample

--- ThreadPriorityExample.java
+++ ThreadPriorityExample.java
@@ -6,3 +6,8 @@ public void run()
 {  
 System.out.println("Inside the run() method");  
 }  
+public static void main(String argvs[])  
+{  
+ThreadPriorityExample th1 = new ThreadPriorityExample();  
+ThreadPriorityExample th2 = new ThreadPriorityExample();  
+ThreadPriorityExample th3 = new ThreadPriorityExample();  
